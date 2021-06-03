d1ab6de3eddaf1248d4e1fb009d9bae8cba55845
AJSmartyPants <81405582+AJSmartyPants@users.noreply.github.com>
2021-06-03 19:51:11 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,36 @@
+class bob
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:1,
+			friction:0,
+			density:0.8
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image = loadImage("ball.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;
+			push()
+			translate(paperpos.x, paperpos.y);
+			ellipseMode(CENTER)
+
+			ellipse(0,0,40, 40);
+			imageMode(CENTER);
+			image(this.image, 0,0,40,40)
+			pop()
+			
+	}
+
+}
+
